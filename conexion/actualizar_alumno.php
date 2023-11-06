3ca487cac8b122f0acfbd686c4bcd3c0cb1853a0
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-11-06 18:41:13 +0100

    Update actualizar_alumno.php

--- conexion/actualizar_alumno.php
+++ conexion/actualizar_alumno.php
@@ -1,3 +1,20 @@
 <?php
-var_dump($_POST);   //MUESTRA LOS VALORES TAL CUAL EN EL ARRY
+include("./funciones.php");
+
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$dni = $_POST["dni"];
+$telefono = $_POST["telefono"];
+$id_alumno = $_POST["id_alumno"];
+
+$conexion = new conectar_db();
+
+$sql = "UPDATE alumnos SET nombre = '$nombre',apellidos ='$apellidos', dni ='$dni',telefono= '$telefono' WHERE id_alumno = $id_alumno";
+
+$resultado = $conexion->consultar($sql);
+
+volver();
+
+
+//var_dump($_POST);   //MUESTRA LOS VALORES TAL CUAL EN EL ARRY
 ?>
