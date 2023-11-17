0bf881e3e1d47060ab72f6213986710e9e9d57d7
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-11-17 14:20:58 +0100

    jgh

---
+++ tiendaVirtual/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div>
+            <h1 id="logo">𝔓ℜℑ𝔐𝔒 </h1>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li style="list-style-type: none;"> <a href="registrar.php">Registro</a></li>
+                    <li style="list-style-type: none;"> <a href="login.php">Login</a></li>
+                    <li style="list-style-type: none;"> <a href="carrito.php"><i class="bi bi-cart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg></i></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <?php
+
+include("./funciones.php");
+
+$conexion = new conectar_db;
+$sql ="SELECT * FROM productos"; //consulta
+
+$resultado = $conexion->consultar($sql);
+
+?>
+<!-- crear una tabla para los resultados -->
+<table class="table table-striped">
+    <tr>
+        <td>ID</td>
+        <td>Nombre</td>
+        <td>Decripcion</td>
+        <td>Precio</td>
+        <td>Categoria</td>
+        
+</tr>
+<?php
+
+while($row = $resultado->fetch_array()){
+    $categoria_query = "SELECT nombre FROM categorias WHERE id_categoria = " . $row["categoria"];
+    $categoria_result = $conexion->consultar($categoria_query);
+    $categoria_row = $categoria_result->fetch_array();
+    $nombre_categoria = $categoria_row["nombre"];
+    echo "<tr>
+            <td>".$row["id"]."</td> 
+            <td>".$row["nombre"]."</td>
+            <td>".$row["descripcion"]."</td>
+            <td>".$row["precio"]."</td>
+            <td>".$nombre_categoria."</td>
+            <td>
+            <a href='añadirCarro.php?id=".$row["id"]."'>
+            <i class='bi bi-cart'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-cart' viewBox='0 0 16 16'>
+            <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z'/></a>
+            
+            </td>
+            </tr>";
+                  // Libera los resultados de la consulta de la categoría
+        mysqli_free_result($categoria_result);
+}
+?>
+</table>
+
+</body>
+</html>
---
+++ tiendaVirtual/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="login-container">
+        <h2>USER LOGIN</h2><br>
+        <form class="form-inline" action="" method="post">
+            <div class="form-group">
+                <label for="usuario">
+                    <img src="usu.png" alt="Usuario" style="background-color: #d3d3d3; padding: 8px; border-radius: 50%; margin-right: 8px;">
+                </label>
+                <input type="text" name="usuario" id="usuario" placeholder="Usuario" required>
+            </div>
+            <div class="form-group">
+                <label for="contrasena">
+                    <img src="R.gif" alt="Contrasena" style="background-color: #d3d3d3; padding: 15px; border-radius: 33%; margin-right: 10px;">
+                </label>
+                <input type="password" name="contrasena" id="contrasena" placeholder="Contrasena" required>
+            </div>
+            <div class="form-group">
+                <input class="submit-btn" type="submit" value="Iniciar sesión">
+            </div>
+</form> 
+</div>
+<?php
+include "funciones.php";
+
+//crear instancia clase conectar
+$conexion_db = new conectar_db();
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $usuario = $_POST["usuario"];
+    $contrasena = $_POST["contrasena"];
+
+    //verificar si el usuario exixtse
+    $consulta= "SELECT id FROM usuarios WHERE nombre = '$usuario' AND contrasena = '$contrasena'";
+
+    if ($conexion_db -> contar_resultados($consulta)>0){
+        //usuario existe
+        echo "¡Inicio de sesion con exito!";
+    }else{
+        echo "El usuario no existe. Registrate!";
+    }
+
+    //cerrar conexion
+
+    $conexion_db -> cerrar();
+
+}
+?>
+</body>
+</html>
