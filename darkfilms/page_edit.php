59fbb3979fef9ae3f3b74b4a5cadc500476a7c40
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-11-24 14:22:00 +0100

    jhl

--- darkfilms/page_edit.php
+++ darkfilms/page_edit.php
@@ -1,7 +1,112 @@
-<?php
+<!-- FALTA AÑADIR DESCRIPCION -->
 
-session_start();
+<?php
+include "funciones.php";
+
+//instancia clase conectar
+$conexion_db = new conectar_db();
+
+//AÑADIR ENTRADA
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['agregar_entrada'])){
+    $titulo = $_POST['titulo'];
+    $fecha_creacion= date ('Y-m-d'); //FECHA ACTUAL
+    $categoria_id = $_POST['categoria'];
+
+    //validar no hay campos vacios
+    if (!empty($titulo) && !empty($categoria_id)){
+        $sql = "INSERT INTO entradas (titulo, fecha_creacion, categoria_id)
+         VALUES ('$titulo', '$fecha_creacion', '$categoria_id')";
+
+         $conexion_db-> consultar($sql);
+
+         echo "Entada agregada con exito";
+    } else{
+        echo "completa todos los campos";
+    }
+}
+
+//EDITAR ENTRADA
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['editar_entrada'])){
+    $entrada_id = $_POST ['entrada_id'];
+    $titulo = $_POST['titulo'];
+    $categoria_id = $_POST['categoria'];
+
+    //validar no hay campos vacios
+    if (!empty($titulo) && !empty($categoria_id)){
+        $sql = "UPDATE entradas SET titulo = '$titulo',categoria_id = '$categoria_id' 
+        WHERE id = '$entrada_id'";
+
+         $conexion_db-> consultar($sql);
+
+         echo "Entada editada con exito";
+    } else{
+        echo "completa todos los campos";
+    }
+}
+
+//ELIMINAR ENTRADA
+if (isset($GET['borrar_entrada'])){
+
+    $entrada_id = $_GET['borrar_entrada'];
+    $sql = "DELETE FROM entradas WHERE id='$entrada_id'";
+
+    $conexion_db-> consultar($sql);
+
+    echo "Entrada eliminada con exito";
+}
+
+//AÑADIR NUEVA CATEGORIA
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['agregar_categoria'])){
+
+    $nombre_categoria = $_POST['nombre_categoria'];
+
+    if(!empty($nombre_categoria)){
+        $sql= "INSERT INTO categorias (nombre) VALUES ('$nombre_categoria')";
+
+        $conexion_db-> consultar($sql);
+
+        echo "Categoria añadida con exito";
+    } else{
+        echo "el campo esta vacio";
+    }
+}
+
+//EDITAR CATEGORIA
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['editar_categoria'])){
+
+    $categoria_id = $_POST['categoria_id'];
+    $nombre_categoria = $_POST['nombre_categoria'];
+
+    if (!empty($nombre_categoria)){
+        $sql = "UPDATE categorias SET nombre = '$nombre_categoria'
+        WHERE id = '$categoria_id'";
+
+        $conexion_db-> consulta($sql);
+
+        echo "Categoria editada con exito";
+    } else {
+        echo "el campo esta vacio";
+    }
+}
+
+//BORRAR CATEGORIA
+if (isset($GET['borrar_categoria'])){
+
+    $categoria_id = $_GET['borrar_categoria'];
+    $sql = "DELETE FROM categorias WHERE id='$categoria_id'";
+
+    $conexion_db-> consultar($sql);
+
+    echo "Categoria eliminada con exito";
+}
+
+//obtener entradas y categorias de la bbdd
+$entradas = $conexion_db->("SELECT * FROM entradas ORDER BY fecha_creacion DESC");
+$categorias = $conexion_db->("SELECT * FROM categorias");
+
 ?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -50,7 +155,30 @@ header {
         </div>
     </header>
 
- 
+ <div>
+    <h2>Administrar entradas del blog </h2>
+
+    <!-- Añadir nueva entrada -->
+    <form method="post">
+    <label for="titulo">Titulo: </label>
+    <input type="text" name="titulo" required>
+
+    <label for="categoria">Categoria: </label>
+    <select name="categoria" required>
+
+    <?php
+    foreach ($categorias as $categoria){
+        echo '<option value="' . $categoria['id'] .'">' . $categoria['nombre'] . '</option>'; 
+    }
+    ?>
+    </select>
+
+    <button type="submit" name="agregar_entrada">Agregar entrada </button>
+</form>
+
+ <!-- Mostrar entradas -->
+
+            </div>
 
     <footer>
         <p>&copy; 2023 Blog de Películas</p>
